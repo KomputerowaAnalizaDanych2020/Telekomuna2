299cefa607bb12c10005e740c86edf57f20c372e
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-04-18 14:26:20 +0200

    start1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+import serial
+
+ser = serial.Serial(
+    port='COM2',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+
+
+if ser.isOpen():
+    print(ser.name + ' is open...')
+
+value = 1
+
+while True:
+    ser.write(str.encode(str(value)))
+    value = int(input("Give me a number from 0 to 9: "))
