138616d7f55044d57f212ea96b9e7a32b23a56c2
maldo31 <74554376+maldo31@users.noreply.github.com>
2021-04-19 01:04:07 +0200

    splitting, crc, send packet works

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import serial
+ser = serial.Serial(
+    port='COM2',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
-
-def crc16(data: bytes, poly=0x8408):
+def crc16(data: bytearray, poly=0x8408):
     crc = 0xFFFF
     for b in data:
         cur_byte = 0xFF & b
@@ -15,25 +22,36 @@ def crc16(data: bytes, poly=0x8408):
     crc = (crc << 8) | ((crc >> 8) & 0xFF)
 
     return crc & 0xFFFF
+def split_data(data_bytes):
+    packets=[]
+    bytearray
+    for packetnr in range(int(len(data_bytes)/128)):
+        packetarray = bytearray()
+        for byte in range(128):
+            packetarray.append(data_bytes[byte*packetnr])
+        packets.append(packetarray)
+    return packets
+
 
 def read_file(path):
     f = open(path, "rb")
-    return f
+    bytesfile=f.read()
+    return bytesfile
 
-path='test.txt'
+def send_packet(packet_to_send):
+    ser.write(packet_to_send)
 
-ser = serial.Serial(
-    port='COM2',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
 
+
+path='test1.bmp'
 databytes=read_file(path)
-sumacrc=crc16(databytes)
-print(sumacrc)
+returnetpackets=split_data(databytes)
+for bitpack in returnetpackets:
+    #print(bitpack)
+    #print(crc16(bitpack))
+    send_packet(bitpack)
+
+"""
 if ser.isOpen():
     print(ser.name + ' is open...')
 
@@ -42,3 +60,4 @@ value = 1
 while True:
     ser.write(str.encode(str(value)))
     value = int(input("Give me a number from 0 to 9: "))
+"""
