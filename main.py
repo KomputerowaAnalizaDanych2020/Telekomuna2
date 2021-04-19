0a395ae0126ff78e7cd1c630b9fe807f78487fb7
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-04-19 21:10:45 +0200

    both modes works

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ ACK=bytearray.fromhex("06")
 NAK=bytearray.fromhex("15")
 CAN=bytearray.fromhex("18")
 C=bytearray.fromhex("43")
+
 def checksuma(data: bytearray):
     temp_sum =0
     for byte in data:
         temp_sum+=int(byte)
-        #print(temp_sum)
     return temp_sum % 256
+
 
 def crc16(data: bytearray, poly=0x1021):
     crc = 0x0000
@@ -35,8 +36,9 @@ def crc16(data: bytearray, poly=0x1021):
             cur_byte >>= 1
     crc = (~crc & 0xFFFF)
     crc = (crc << 8) | ((crc >> 8) & 0xFF)
-
     return crc & 0xFFFF
+
+
 def split_data(data_bytes):
     packets=[]
     for packetnr in range(int(len(data_bytes)/128) + (len(data_bytes)%128 > 0)):
@@ -55,19 +57,22 @@ def read_file(path):
     bytesfile=f.read()
     return bytesfile
 
-def send_packet(packet_to_send,numberp):
+
+def send_packet(packet_to_send,numberp,mode):
+    crcinst = CrcXmodem()
     while True:
         header = bytearray()
         header.append(int.from_bytes(SOH,'big'))
         header.append(numberp+1)
         header.append(254-numberp)
-        suma=crcinst.final()
-        crcinst.process(packet_to_send)
-        print(suma)
-        print(crcinst.finalhex())
         full=header+packet_to_send
-        full.append((suma>>8) & 0xff)
-        full.append(suma & 0xff)
+        if mode==C:
+            suma = crcinst.final()
+            crcinst.process(packet_to_send)
+            full.append((suma>>8) & 0xff)
+            full.append(suma & 0xff)
+        if mode==NAK:
+            full.append(checksuma(packet_to_send))
         ser.write(full)
         print(full)
         print(len(full))
@@ -79,32 +84,21 @@ def send_packet(packet_to_send,numberp):
         if answer == CAN:
             break
 
-        #print(numberp)
+path = 'test1.bmp'
 
-crcinst=CrcXmodem()
-path='test.txt'
-databytes=read_file(path)
-returnetpackets=split_data(databytes)
-packet_number=0
-for bitpack in returnetpackets:
-    #print(bitpack)
-    #print(crc16(bitpack))
-    while 1:
-
-        if ser.read()==C:
-            print(ser.read())
-            send_packet(bitpack,packet_number)
-            break
-    packet_number += 1
-ser.write(EOT)
-
-"""
-if ser.isOpen():
-    print(ser.name + ' is open...')
-
-value = 1
-
-while True:
-    ser.write(str.encode(str(value)))
-    value = int(input("Give me a number from 0 to 9: "))
-"""
+def send_data(path):
+    databytes = read_file(path)
+    returnetpackets = split_data(databytes)
+    packet_number = 0
+    initialAnswer=ser.read()
+    while initialAnswer!=C and initialAnswer!=NAK:
+        initialAnswer=ser.read()
+        print(initialAnswer)
+        continue
+    mode=initialAnswer
+    for bitpack in returnetpackets:
+        ser.flush()
+        send_packet(bitpack,packet_number,mode)
+        packet_number += 1
+    ser.write(EOT)
+send_data(path)
